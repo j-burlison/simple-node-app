ce3ead1092e5c84b575d4b1f8f9033e7ca92d672
j-burlison <jburliso@redhat.com>
2018-01-29 17:37:39 -0600

    tt

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/', (req, res, next)=>{
   res.send('SERVICE HIT: '+req.url);
 });
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8090,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.listen(port, ip);
