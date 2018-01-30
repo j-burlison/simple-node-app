793f85ad137dd58a76513db38d3d5dcad3c7728e
j-burlison <jburliso@redhat.com>
2018-01-30 10:42:48 -0600

    Switched Port

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 // LOCALHOST Port
-var port = 8090;
+// var port = 8090;
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
